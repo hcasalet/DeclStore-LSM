fd86f782d0934a38634bb3972263cb62ecfea2d7
Holly Casaletto <hollycasaletto@Hollys-MacBook-Pro-2.local>
2021-07-09 22:28:48 -0600

    create repository

---
+++ cmake-build-debug/CMakeFiles/DeclStore_LSM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeclStore_LSM.dir/main.cpp.o"
+  "DeclStore_LSM"
+  "DeclStore_LSM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DeclStore_LSM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/DeclStore_LSM.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DeclStore_LSM.
+# This may be replaced when dependencies are built.
